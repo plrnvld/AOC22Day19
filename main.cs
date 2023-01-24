4b8f8601cb877c83821bf1082de879cda22f6ed2
Paul Rijneveld <paulrijneveld@gmail.com>
2023-01-24 20:27:33 +0000

    Add MoveList, Robot and Move

--- main.cs
+++ main.cs
@@ -9,13 +9,13 @@ class Program
     {
         foreach (var bp in ReadBlueprints("Example.txt"))
         {
-            Console.WriteLine(bp);            
+            Console.WriteLine(bp);
         }
     }
 
-    static IEnumerable<Blueprint> ReadBlueprints(string file) => 
+    static IEnumerable<Blueprint> ReadBlueprints(string file) =>
         File.ReadLines(file).Select(Blueprint.From);
-    
+
 }
 
 record struct Blueprint(int Num, int OreRobotOrePrice, int ClayRobotOrePrice, int ObsidianRobotOrePrice, int ObsidianRobotClayPrice, int GeodeRobotOrePrice, int GeodeRobotObsidianPrice)
@@ -27,4 +27,33 @@ record struct Blueprint(int Num, int OreRobotOrePrice, int ClayRobotOrePrice, in
 
         return new Blueprint(int.Parse(words[1][..^1]), Word(6), Word(12), Word(18), Word(21), Word(27), Word(30));
     }
+}
+
+record struct MoveList(IList<Move> Moves)
+{
+    static Robot[] allRobots = new Robot[] { Robot.Ore, Robot.Clay, Robot.Obsidian, Robot.Geode };
+    
+    public IEnumerable<Robot> AllowedToBuy()
+    {
+        var hasClay = Moves.Any(m => m.Buy == Robot.Clay);
+        var hasObsidian = Moves.Any(m => m.Buy == Robot.Obsidian);
+        
+        if (!hasClay)
+            return allRobots[0..2];            
+
+        if (hasClay && !hasObsidian)
+            return allRobots[0..3];
+
+        return allRobots[0..4];        
+    }
+}
+
+record struct Move(Robot Buy, int Num);
+
+enum Robot
+{
+    Ore,
+    Clay,
+    Obsidian,
+    Geode    
 }
